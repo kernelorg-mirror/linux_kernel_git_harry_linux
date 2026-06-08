42ae4e6d1ea16e0fb08c010fa4f4e2de14ecc3cc
Harry Yoo (Oracle) <harry@kernel.org>
2026-06-08 14:04:39 +0900

    mm/slab: extend deferred free mechanism to handle rcu sheaves
    __kfree_rcu_sheaf() cannot invoke call_rcu() when spinning is not
    allowed. To relax the limitation, extend the deferred free fallback
    so that a full rcu sheaf can be submitted to call_rcu() via the existing
    IRQ work.
    
    Since the deferred mechanism does more than deferred free of objects,
    rename it to the struct to deferred_percpu_work and adjust names
    accordingly.
    
    When a sheaf is queued on an IRQ work, it is detached from
    pcs->rcu_sheaf but call_rcu() is not invoked until the irq_work runs.
    To keep the kvfree_rcu barrier's promise, call irq_work_sync() on each
    CPU before calling rcu_barrier().
    
    In the meantime, remove the TODO item as apparently there is no simple
    and effective way to achieve that.
    
    Signed-off-by: Harry Yoo (Oracle) <harry@kernel.org>

--- mm/slab_common.c
+++ mm/slab_common.c
@@ -551,7 +551,7 @@ void kmem_cache_destroy(struct kmem_cache *s)
 	}
 
 	/* Wait for deferred work from kmalloc/kfree_nolock() */
-	defer_free_barrier();
+	deferred_work_barrier();
 
 	cpus_read_lock();
 	mutex_lock(&slab_mutex);
@@ -2112,14 +2112,11 @@ void kvfree_rcu_barrier_on_cache(struct kmem_cache *s)
 	if (cache_has_sheaves(s)) {
 		cpus_read_lock();
 		flush_rcu_sheaves_on_cache(s);
+		deferred_work_barrier();
 		cpus_read_unlock();
 		rcu_barrier();
 	}
 
-	/*
-	 * TODO: Introduce a version of __kvfree_rcu_barrier() that works
-	 * on a specific slab cache.
-	 */
 	__kvfree_rcu_barrier();
 }
 EXPORT_SYMBOL_GPL(kvfree_rcu_barrier_on_cache);
